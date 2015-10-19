7a1dec105a735ba7af52c6477779080fdcd0621b
Denchik <noobskill1@gmail.com>
2015-10-19 20:08:01 +0400

    Создали границы

---
+++ Zmeya/Zmeya_osn/Zmeyka/Zmeyka/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmeyka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3,5,'#');
+            p1.Draw();
+
+            Point p2 = new Point(5, 5, '!');
+            p2.Draw();
+
+            VerticalLine line = new VerticalLine(6, 10, 1, '|');
+            line.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
